ddb3617844213c4c8690633f998b8f9270be323f
trisnobp <trisnotyo6@gmail.com>
2024-05-28 18:54:59 +0700

    feat: add splash screen

--- app/src/main/java/com/dicoding/storyapp/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dicoding.storyapp
+package com.dicoding.storyapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.storyapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/dicoding/storyapp/view/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.dicoding.storyapp.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.dicoding.storyapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, TIME_IN_MILLIS)
+    }
+
+    companion object {
+        const val TIME_IN_MILLIS : Long = 3000
+    }
+}
